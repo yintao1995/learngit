f7e3937f7856ff2d73269738a00a787ce9b38b44
yintao1995 <yintao.pku@gmail.com>
2019-05-18 09:25:53 +0800

    Add files via upload

---
+++ read2.py
@@ -0,0 +1,22 @@
+import time
+import struct
+import sys
+import numpy as np
+
+header = int(32768/2)
+samples = 520
+lines = 696
+bands = 128
+datalength = lines*bands*samples
+t1 = time.time()
+
+filename = 'C:/Users/YINTAO/Desktop/test.cube'
+data_list = np. fromfile(filename, 'uint16')[header: header+datalength]
+print(len(data_list))
+print(data_list[:10])
+print(sys.getsizeof(data_list[0]))
+print(sys.getsizeof(data_list[9]))
+t2 = time.time()
+print(t2-t1)
+a = input('quit?')
+print(a)
