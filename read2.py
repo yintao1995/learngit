6761f07ebff90dde4f3cb7c07e8877b735ac900a
yintao1995 <yintao.pku@gmail.com>
2019-05-18 09:41:34 +0800

    Update read2.py

--- read2.py
+++ read2.py
@@ -11,7 +11,8 @@ datalength = lines*bands*samples
 t1 = time.time()
 
 filename = 'C:/Users/YINTAO/Desktop/test.cube'
-data_list = np. fromfile(filename, 'uint16')[header: header+datalength]
+# data_list = np. fromfile(filename, 'uint16')[header: header+datalength]
+data_list = np. fromfile(filename, dtype='<u2', )[header: header+datalength]
 print(len(data_list))
 print(data_list[:10])
 print(sys.getsizeof(data_list[0]))
